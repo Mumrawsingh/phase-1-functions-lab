0922f7b6197d8466c735ab91a79f2d429f0a7cf5
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-11-02 22:21:44 -0400

    commit tree 1ae735e639d0133d65475e0b803c5ce694eec348

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if  (block < 42) {
+       return 42 - block; 
+   } else if (block >= 42) {
+       return block - 42 
+   }
+}
+
+function distanceFromHqInFeet(block) {
+    let blocks = distanceFromHqInBlocks(block)
+        return blocks * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+         return Math.abs(destination-start) * 264                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+}
+
+function calculatesFarePrice(start,destination) {
+    let distance = distanceTravelledInFeet(start,destination);
+    if ((distance > 400) && (distance <= 2000)) {
+        return (distance - 400) * .02
+    } else if (distance <=400) {
+        return 0
+    } else if ((distance > 2000) && (distance <= 2500)) {
+        return 25
+    } else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
